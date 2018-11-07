6c3bd50a0b6e0ff1da8af5abbc1de985ec18ed23
mistyhippo <Valentinabrent@gmail.com>
2018-11-07 14:17:28 -0800

    create and launch second activity

--- activities-and-intents/app/src/main/java/mistyhippo/github/com/twoactivities/MainActivity.java
+++ activities-and-intents/app/src/main/java/mistyhippo/github/com/twoactivities/MainActivity.java
@@ -1,13 +1,24 @@
 package mistyhippo.github.com.twoactivities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchSecondActivity(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+        Log.d(LOG_TAG, "Button clicked!");
     }
 }
