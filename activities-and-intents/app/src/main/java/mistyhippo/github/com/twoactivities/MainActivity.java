5a441ecf1e6ca79edc5515c524c30ffc5de657a5
mistyhippo <Valentinabrent@gmail.com>
2018-11-07 17:01:00 -0800

    send data from the main activity to the second activity

--- activities-and-intents/app/src/main/java/mistyhippo/github/com/twoactivities/MainActivity.java
+++ activities-and-intents/app/src/main/java/mistyhippo/github/com/twoactivities/MainActivity.java
@@ -5,19 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE = "mistyhippo.github.com.twoactivities";
+    private EditText mMessageEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMessageEditText = findViewById(R.id.editText_main);
     }
 
     public void launchSecondActivity(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
         Log.d(LOG_TAG, "Button clicked!");
     }
