a049defd9fe165bafb810b0285d9368f886bfbb9
mistyhippo <Valentinabrent@gmail.com>
2018-11-09 20:26:53 -0800

    implement button behavior

--- support-libraries/app/src/main/java/mistyhippo/github/com/hellocompat/MainActivity.java
+++ support-libraries/app/src/main/java/mistyhippo/github/com/hellocompat/MainActivity.java
@@ -1,8 +1,12 @@
 package mistyhippo.github.com.hellocompat;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,5 +32,14 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
         // save the current text color
         outState.putInt("color", mHelloTextView.getCurrentTextColor());
+    }
+
+    public void changeColor(View view) {
+        Random random = new Random();
+        String colorName = mColorArray[random.nextInt(20)];
+        int colorResourceName = getResources().getIdentifier(colorName, "color",
+                getApplicationContext().getPackageName());
+        int colorRes = ContextCompat.getColor(this, colorResourceName);
+        mHelloTextView.setTextColor(colorRes);
     }
 }
